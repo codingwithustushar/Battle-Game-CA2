9d8de6852398838c908a50c1c04a7f72d343f9f6
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2023-11-24 09:36:06 +0530

    hi

---
+++ nickname.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const playerNameInput = document.getElementById("playerName");
+    const nicknameInput = document.getElementById("nickname");
+    const startButton = document.getElementById("startit");
+    const nicknameForm = document.getElementById("nicknameForm");
+
+    // Function to enable/disable the start button based on input validity
+    function updateStartButton() {
+        startButton.disabled = !nicknameForm.checkValidity();
+    }
+
+    // Add event listeners for input changes
+    playerNameInput.addEventListener("input", updateStartButton);
+    nicknameInput.addEventListener("input", updateStartButton);
+
+    // Add submit event listener to prevent the form submission if inputs are not valid
+    nicknameForm.addEventListener("submit", function (event) {
+        if (!nicknameForm.checkValidity()) {
+            event.preventDefault();
+        }
+    });
+});
+
+
+const goback = document.getElementById("goback");
+
+goback.addEventListener("click",function(){
+    window.location.href="./index.html"
+})
+const start = document.getElementById("startit");
+
+start.addEventListener("click",function(){
+    window.location.href="./game.html"
+})
